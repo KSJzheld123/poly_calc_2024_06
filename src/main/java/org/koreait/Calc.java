0c229a76e1c483ee2dd9d627b854dd9ad38ef830
ksjzheld123 <rlatjswnzheld970616@gmail.com>
2024-06-24 16:35:54 +0900

    0.5

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -8,36 +8,43 @@ import java.util.stream.Stream;
 public class Calc {
     public static int run(String exp) {
 
-        boolean needToPlus = exp.contains("+");
-        boolean needToMinus = exp.contains("-");
+//        boolean needToPlus = exp.contains("+");
+//        boolean needToMinus = exp.contains("-");
 
+        String[] bits = exp.split(" ");
+        int result = 0;
+        result = Integer.parseInt(bits[0]);
 
-        String[] bits = null;
-        int c = 0;
-
-
-        if(needToPlus) {
-            bits = exp.split(" \\+ ");
-        } else if(needToMinus) {
-            bits = exp.split(" - ");
+        for(int a = 1; a < bits.length; a++) {
+            if (bits[a].contains("+")) {
+                result += Integer.parseInt(bits[a+1]);
+            } else if(bits[a].contains("-")) {
+                result -= Integer.parseInt(bits[a+1]);
+            }
         }
+        return result;
 
-        if(bits.length > 2) {
-            c = Integer.parseInt(bits[2]);
-        }
-
-        if(needToPlus) {
-            int a = Integer.parseInt(bits[0]);
-            int b = Integer.parseInt(bits[1]);
-            return a + b + c;
-        } else if(needToMinus) {
-            int a = Integer.parseInt(bits[0]);
-            int b = Integer.parseInt(bits[1]);
-            return a - b - c;
-        }
-
-        throw new RuntimeException("해석 불가");
+//        int c = 0;
+//
+//        if(needToPlus) {
+//            bits = exp.split(" \\+ ");
+//        } else if(needToMinus) {
+//            bits = exp.split(" - ");
+//        }
+//
+//        if(bits.length > 2) {
+//            c = Integer.parseInt(bits[2]);
+//        }
+//
+//        if(needToPlus) {
+//            int a = Integer.parseInt(bits[0]);
+//            int b = Integer.parseInt(bits[1]);
+//            return a + b + c;
+//        } else if(needToMinus) {
+//            int a = Integer.parseInt(bits[0]);
+//            int b = Integer.parseInt(bits[1]);
+//            return a - b - c;
+//        }
 
     }
-
 }
--- src/test/java/org/koreait/CalcTests.java
+++ src/test/java/org/koreait/CalcTests.java
@@ -55,6 +55,18 @@ public class CalcTests {
         assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
     }
 
+    @Test
+    @DisplayName("10 - 20 + 30 == 20")
+    void t9() {
+        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 = -10")
+    void t10() {
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+    }
+
 
 
 }
