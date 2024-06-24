7eedab7e6addeb318eb2514c40a7acb2f06d12dc
ksjzheld123 <rlatjswnzheld970616@gmail.com>
2024-06-24 15:26:45 +0900

    0.4

--- src/main/java/org/koreait/Calc.java
+++ src/main/java/org/koreait/Calc.java
@@ -1,12 +1,20 @@
 package org.koreait;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Calc {
     public static int run(String exp) {
 
         boolean needToPlus = exp.contains("+");
         boolean needToMinus = exp.contains("-");
 
+
         String[] bits = null;
+        int c = 0;
+
 
         if(needToPlus) {
             bits = exp.split(" \\+ ");
@@ -14,14 +22,18 @@ public class Calc {
             bits = exp.split(" - ");
         }
 
+        if(bits.length > 2) {
+            c = Integer.parseInt(bits[2]);
+        }
+
         if(needToPlus) {
             int a = Integer.parseInt(bits[0]);
             int b = Integer.parseInt(bits[1]);
-            return a + b;
+            return a + b + c;
         } else if(needToMinus) {
             int a = Integer.parseInt(bits[0]);
             int b = Integer.parseInt(bits[1]);
-            return a - b;
+            return a - b - c;
         }
 
         throw new RuntimeException("해석 불가");
